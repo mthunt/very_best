726bf288b85c7e6703c3aeb0244e449fa48e124f
Raghu Betina <raghu@firstdraft.com>
2018-04-01 21:57:04 +0000

    Whitelist columns in Venue for ActiveAdmin

--- app/admin/venues.rb
+++ app/admin/venues.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Venue do
+
+ permit_params :location, :address, :neighborhood
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
